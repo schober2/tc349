d6bc36668fbdbe09968c80ccd2203d1531ab5168
schober2 <schober2@msu.edu>
2013-05-03 22:36:39 -0400

    Final (site at https://www.egr.msu.edu/~schober2/

---
+++ projects/final/js/script.js
@@ -0,0 +1,8 @@
+function contact()
+{
+  alert("For contact information, please see the Michigan State University people search.");
+}
+function about()
+{
+  alert("This site was built to demonstrate a functional mobile site and my awesomeness.");
+}
